9b3a80ccae2ef420a9bba8de4c155301a43eecb6
unknown <natalivelezpolania04@gmail.com>
2020-09-03 16:05:40 -0500

    De nuevo

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,3 +5,37 @@ router.get('/',(req,res)=>{
 
     res.render('index')
 })
+
+
+router.get('/menu',(req,res)=>{
+    res.render('menu')
+})
+
+router.get('/nosotros',(req,res)=>{
+    res.render('sobreNosotros')
+})
+
+router.get('/sucursales',(req,res)=>{
+    res.render('sucursales')
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+router.get('/crearcuenta',(req,res)=>{
+    res.render('createAccount')
+})
+
+
+//posts
+
+router.post('/crearcuenta',(req,res)=>{
+    console.log(req.body)
+    db.collection('algo').doc('12344').set(req.body)
+    .then(()=>{
+        console.log('dato almacenado')
+    })
+})
+
+module.exports=router;
